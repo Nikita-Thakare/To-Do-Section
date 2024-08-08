72dd6f24f3f96090d606fdb2b82c08edcaecbf8d
mukeshkr2024 <Nikita.Thakare@cloudprism.in>
2024-08-08 11:09:54 +0530

    To-DO

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./details.html", // Includes all HTML files in the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
